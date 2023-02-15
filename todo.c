ecc5de078f89c3da99d1edadc1a5612a4084d8a7
Ethan Shoham <Ethan@atera.com>
2023-02-15 18:21:56 +0200

    add json parser

--- todo.c
+++ todo.c
@@ -5,201 +5,280 @@
 #include <stdbool.h>
 #include <time.h>
 
-typedef struct timespec Todo_DateTime;
-typedef struct timespec Todo_TimeSpan;
+typedef struct todo_task_arr
+{
+    struct todo_task *values;
+    uint32_t length;
+    uint32_t capacity;
+};
 
-typedef struct todo_task {
-    uint32_t index;
-    char* task;
-    Todo_DateTime etc_utc;
+typedef struct todo_task
+{
+    uint32_t id;
+    char *task;
+    time_t etc_utc;
     bool completed;
-    
-    char* description;
-    struct todo_task *sub_tasks;
-
+    char *description;
+    struct todo_task_arr sub_tasks;
 } Todo_Task;
 
+size_t time_to_sorted_str(char *buffer, size_t size_in_bytes, time_t datetime)
+{
+    if (size_in_bytes < sizeof(char) * 20)
+        return 0;
 
-#define MAX_NANO_SEC 999999999
-Todo_TimeSpan todo_timespan_from_nano(int64_t nano_sec){
-    Todo_TimeSpan ts = {0, 0};
-    if(nano_sec < 0)
-    {
-
-    }else{
-        ts.tv_sec = nano_sec / MAX_NANO_SEC;
-        ts.tv_nsec = nano_sec % MAX_NANO_SEC;
-    }
-
-    return ts;
+    struct tm bdt;
+    gmtime_s(&bdt, &datetime);
+    return strftime(buffer, size_in_bytes, "%Y-%m-%dT%H:%M:%S", &bdt);
 }
 
-Todo_TimeSpan todo_timespan_from_mil(int64_t mil_sec){
-    return todo_timespan_from_nano(mil_sec * 1000000);
-}
-
-Todo_TimeSpan todo_timespan_from_sec(int64_t sec){
-    Todo_TimeSpan ts = {sec, 0};
-    return ts;
-}
-Todo_TimeSpan todo_timespan_from_min(int64_t min){
-    return todo_timespan_from_sec(min*60);
-}
-Todo_TimeSpan todo_timespan_from_hours(int64_t hours){
-    return todo_timespan_from_min(hours*60);
-}
-Todo_TimeSpan todo_timespan_from_days(int64_t days){
-    return todo_timespan_from_hours(days*24);
-}
-
-Todo_DateTime todo_datetime_utc_now(void){
-    Todo_DateTime tt;
+time_t time_utc_now(void)
+{
+    struct timespec tt;
     timespec_get(&tt, TIME_UTC);
-    return tt;
-}
-void todo_datetime_add(Todo_DateTime *tt, Todo_TimeSpan time_span){
-    time_t sec = tt->tv_sec + time_span.tv_sec;
-    int reminder;
-    long nano_sec;
-
-    if(time_span.tv_sec < 0)
-    {
-        nano_sec = tt->tv_nsec - time_span.tv_nsec;
-        reminder = nano_sec < 0 ? -1 : 0;
-        nano_sec = nano_sec < 0 ? MAX_NANO_SEC + nano_sec : nano_sec; 
-    }else{
-        nano_sec = tt->tv_nsec + time_span.tv_nsec;
-        reminder = nano_sec / MAX_NANO_SEC;
-        nano_sec = nano_sec % MAX_NANO_SEC; 
-    }
-
-    sec += reminder;
-    tt->tv_nsec = nano_sec;
-    tt->tv_sec = sec;
+    return tt.tv_sec;
 }
 
-Todo_Task todo_task(char* task, char* description, Todo_DateTime etc_utc){
+time_t time_add_secs(time_t time, uint64_t sec)
+{
+    return time + sec;
+}
+
+time_t time_add_mins(time_t time, uint64_t min)
+{
+    return time + min * 60;
+}
+
+time_t time_add_hours(time_t time, uint64_t hours)
+{
+    return time + hours * 60 * 60;
+}
+
+time_t time_add_days(time_t time, uint64_t days)
+{
+    return time + days * 24 * 60 * 60;
+}
+
+Todo_Task todo_task(char *task, char *description, time_t etc_utc)
+{
     Todo_Task todo_task = {
-        .index = 0,
+        .id = 0,
         .task = task,
         .description = description,
         .etc_utc = etc_utc,
-        .sub_tasks = NULL,
+        .sub_tasks.values = NULL,
+        .sub_tasks.capacity = 0,
+        .sub_tasks.length = 0,
         .completed = false,
     };
     return todo_task;
 }
 
-// 0000-00-00T00:00:00.000000000\0
-// 
-size_t todo_datetime_to_sorted(char *buffer, size_t size_in_bytes, Todo_DateTime datetime){
-    if(size_in_bytes < 30)
-        return 0;
+void todo_task_free(Todo_Task *task)
+{
+    if (task == NULL)
+        return;
 
-    struct tm bdt;
-    gmtime_s(&bdt,&datetime.tv_sec);
-    size_t pos = strftime(buffer, sizeof(char)*31, "%Y-%m-%dT%H:%M:%S", &bdt);
-    printf("%zu\n", pos);
-    buffer[pos] = '.';
-    _itoa_s(datetime.tv_nsec,buffer + pos + 1, 10, 10);
-    buffer[29] = 0;
-    return 29;
+    for (uint32_t i = 0; i < task->sub_tasks.length; i++)
+        todo_task_destroy(task->sub_tasks.values + i);
+
+    free(task->sub_tasks.values);
+    task->sub_tasks.length = 0;
+    task->sub_tasks.capacity = 0;
 }
 
-int main(int argc, char *argv[]){
+bool todo_task_add_sub_task(Todo_Task *parent, Todo_Task child)
+{
+    if (parent == NULL)
+        return false;
 
-    //int seconds = atoi(argv[1]); 
-    Todo_DateTime ts = todo_datetime_utc_now();
-    //todo_datetime_add(&ts, todo_timespan_from_sec(50));
-    Todo_Task task = todo_task("create remove command", "task description", ts);
-    char time_str[100]; 
+    if (parent->sub_tasks.length + 1 >= parent->sub_tasks.capacity)
+    {
+        uint32_t min_capacity = 4;
+        if (parent->sub_tasks.length + 1 > min_capacity)
+            min_capacity = 2 * parent->sub_tasks.capacity;
+        Todo_Task *tasks = realloc(tasks->sub_tasks.values, sizeof(*tasks) * min_capacity);
 
-    size_t length = todo_datetime_to_sorted(time_str, sizeof time_str, ts);
+        if (tasks == NULL)
+            return false;
+
+        parent->sub_tasks.values = tasks;
+        parent->sub_tasks.capacity = min_capacity;
+    }
+
+    parent->sub_tasks.values[parent->sub_tasks.length++] = child;
+    if (!child.completed)
+        parent->completed = false;
+}
+
+void print_todo_task(Todo_Task *task, uint32_t indention)
+{
+    for (uint32_t i = 0; i < indention; i++)
+        putc('\t', stdout);
+
+    char datetime[20];
+    time_to_sorted_str(datetime, sizeof(*datetime) * 20, task->etc_utc);
+    char *completed_str = task->completed ? "v" : "";
+    printf("[%s] %d: %s - t: %s\n", completed_str, task->id, task->task, datetime);
+
+    for (uint32_t i = 0; i < task->sub_tasks.length; i++)
+        print_todo_task(&task->sub_tasks.values[i], indention + 1);
+}
+
+uint64_t _todo_task_save(Todo_Task *task, const FILE *stream, uint64_t index)
+{
+    char *desc = NULL;
+    bool has_desc = task->description != NULL;
+    if (has_desc)
+    {
+        uint64_t desc_len = strlen(task->description);
+        uint64_t desc_cap = desc_len + 2 + 1;
+        desc = malloc(sizeof(*desc) * desc_cap);
+        if (desc == NULL)
+        {
+            has_desc = false;
+        }
+        else
+        {
+            desc[0] = '\"';
+            for (uint32_t i = 0; i < desc_len; i++)
+                desc[i + 1] = task->description[i];
+            desc[desc_len + 1] = '\"';
+            desc[desc_len + 2] = 0;
+        }
+    }
+
+    uint64_t chars_written = fprintf_s(stream, "{\"id\":\"%zu\",\"task\":\"%s\",\"description\":%s,\"etc_utc\":%zu,\"completed\":%s\"sub_tasks\":[",
+                                  index, task->task, has_desc ? desc : "null",task->etc_utc, task->completed ? "true" : "false");
+    if (has_desc)
+        free(desc);
+
+    for (uint32_t i = 0; i < task->sub_tasks.length; i++)
+        chars_written += _todo_task_save(&task->sub_tasks.values[i], stream, chars_written);
     
-    printf("Current task time: %s UTC\nLength: %zu\n", time_str, length);
+    chars_written += fprintf_s(stream, "]}");
+
+    return chars_written;
+}
+
+bool todo_task_save(Todo_Task *task, const char *filePath)
+{
+    if (task == NULL)
+        return false;
+
+    FILE *fs;
+    errno_t err = fopen_s(&fs, filePath, "w");
+
+    if (err || fs == NULL)
+        return false;
+
+    uint64_t chars = _todo_task_save(task, fs, 0);
+
+    return chars != 0;
+}
+
+int main(int argc, char *argv[])
+{
+    time_t task_time = time_add_days(time_utc_now(), 100);
+    Todo_Task project = todo_task("Build todo cli tool", NULL, task_time);
+
+    char *task_names[5];
+    for (size_t i = 0; i < 5; i++)
+    {
+        task_names[i] = malloc(sizeof(char) * 100);
+        sprintf_s(task_names[i], sizeof(char) * 100, "Sub Task num %d", i + 1);
+        time_t sub_task_time = time_add_days(time_utc_now(), 1 + i);
+        Todo_Task sub_task = todo_task(task_names[i], NULL, sub_task_time);
+        todo_task_add_sub_task(&project, sub_task);
+    }
+
+    print_todo_task(&project, 0);
+
+    for (size_t i = 0; i < 5; i++)
+        free(task_names[i]);
+
+    todo_task_free(&project);
 
     return 0;
 }
 
-
-
-//typedef struct cmd_struct {
-//    const char *cmd;
-//    int (*execute)(int, char **);
-//} Command;
+// typedef struct cmd_struct {
+//     const char *cmd;
+//     int (*execute)(int, char **);
+// } Command;
 //
 //
-//int add_cmd(int argc, char **argv){
+// int add_cmd(int argc, char **argv){
 //
-//    return 0;
-//}
+//     return 0;
+// }
 //
-//int info_cmd(int argc, char **argv){
-//    printf("Todo cli, use -h for more information on how to use it.\n");
+// int info_cmd(int argc, char **argv){
+//     printf("Todo cli, use -h for more information on how to use it.\n");
 //
-//    return 0;
-//}
+//     return 0;
+// }
 //
-//int help_cmd(int argc,const char **argv)
+// int help_cmd(int argc,const char **argv)
 //{
-//    printf("Help!");
+//     printf("Help!");
 //
-//    return 0;
-//}
+//     return 0;
+// }
 //
-//#define COMMANDS_COUNT 2
-//static Command commands[] = {
-//    {"info", info_cmd},
-//    {"add",add_cmd}
-//};
+// #define COMMANDS_COUNT 2
+// static Command commands[] = {
+//     {"info", info_cmd},
+//     {"add",add_cmd}
+// };
 //
-//Command* get_command(int argc, char **argv){
+// Command* get_command(int argc, char **argv){
 //
-//    if(argc < 1)
-//        return NULL;
+//     if(argc < 1)
+//         return NULL;
 //
-//    for (size_t i = 0; i < COMMANDS_COUNT; i++)
-//    {
-//        Command* p = commands + i;
-//        if(!strcmp(p->cmd, argv[0]))
-//            return p;
-//    }
+//     for (size_t i = 0; i < COMMANDS_COUNT; i++)
+//     {
+//         Command* p = commands + i;
+//         if(!strcmp(p->cmd, argv[0]))
+//             return p;
+//     }
 //
-//    return NULL;
-//}
+//     return NULL;
+// }
 //
-//int main(int argc, char **argv){
-//    if(argc == 1)
-//    {
-//        printf("you need help?");
-//        exit(1);
-//    }
+// int main(int argc, char **argv){
+//     if(argc == 1)
+//     {
+//         printf("you need help?");
+//         exit(1);
+//     }
 //
-//    argv++;
-//    argc--;
+//     argv++;
+//     argc--;
 //
-//    if(!strcmp(argv[0], "-h") || !strcmp(argv[0], "--help"))
-//    {
-//        printf("help");
-//        exit(0);
-//    }
+//     if(!strcmp(argv[0], "-h") || !strcmp(argv[0], "--help"))
+//     {
+//         printf("help");
+//         exit(0);
+//     }
 //
-//    if(!strcmp(argv[0], "-v") || !strcmp(argv[0], "--version"))
-//    {
-//        printf("version");
-//        exit(0);
-//    }
+//     if(!strcmp(argv[0], "-v") || !strcmp(argv[0], "--version"))
+//     {
+//         printf("version");
+//         exit(0);
+//     }
 //
-//    Command* command = get_command(argc, argv);
+//     Command* command = get_command(argc, argv);
 //
-//    if(!command)
-//    {
-//        printf("No valid command was entered please -h for help.");
-//        exit(1);
-//    }
+//     if(!command)
+//     {
+//         printf("No valid command was entered please -h for help.");
+//         exit(1);
+//     }
 //
-//    argv++;
-//    argc--;
+//     argv++;
+//     argc--;
 //
-//    exit(command->execute(argc, argv));
-//}
+//     exit(command->execute(argc, argv));
+// }
